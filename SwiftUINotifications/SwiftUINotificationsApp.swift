abdbda99d90804fde55a30b5cf382dcb3b086639
CristianLopes <cristianqibins@hotmail.com>
2023-09-13 23:49:12 -0300

    Initial Commit

---
+++ SwiftUINotifications/SwiftUINotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINotificationsApp.swift
+//  SwiftUINotifications
+//
+//  Created by Cristian Cris on 13/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
