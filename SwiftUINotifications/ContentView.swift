189d3ae1b3564c4868d95044d176980048b8216a
CristianLopes <cristianqibins@hotmail.com>
2023-09-14 01:26:38 -0300

    adding date components

--- SwiftUINotifications/ContentView.swift
+++ SwiftUINotifications/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
         VStack {
             Button(action: {
@@ -23,7 +24,32 @@ struct ContentView: View {
             Button(action: {
                 NotificationManager.instance.scheduleNotification(timeInterval: 5.0, repeats: false)
             }, label: {
-                Text("Schedule a notification")
+                Text("Schedule a notification ")
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(.blue)
+                    .foregroundColor(.white)
+            })
+            
+            Button(action: {
+                var date = Date()
+                print("Date from device: \(date)")
+                
+                date.addTimeInterval(60.0)
+                
+                print("Date from device after change: \(date)")
+                let calendar = Calendar.current
+                let hour = calendar.component(.hour, from: date)
+                let minutes = calendar.component(.minute, from: date)
+                
+                var dateComponents = DateComponents()
+                dateComponents.hour = hour
+                dateComponents.minute = minutes
+                
+                NotificationManager.instance.scheduleNotification(dateComponent: dateComponents, repeats: true)
+                
+            }, label: {
+                Text("Schedule a notification using date")
                     .padding()
                     .frame(maxWidth: .infinity)
                     .background(.blue)
--- SwiftUINotifications/NotificationManager.swift
+++ SwiftUINotifications/NotificationManager.swift
@@ -34,17 +34,34 @@ class NotificationManager {
     }
     
     func scheduleNotification(timeInterval: TimeInterval, repeats: Bool) {
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInterval, repeats: repeats)
+        
+        sendNotification(content: getNotificationContent(title: "Using TimeInterval"), trigger: trigger)
+    }
+
+    func scheduleNotification(dateComponent: DateComponents, repeats: Bool) {
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponent, repeats: repeats)
+        
+        sendNotification(content: getNotificationContent(title: "Using DateComponents"), trigger: trigger)
+    }
+    
+    private func getNotificationContent(title: String) -> UNMutableNotificationContent {
         
         let content = UNMutableNotificationContent()
-        content.title = "Stay hungy, Stay Foolish"
+        content.title = title
+        content.subtitle = "Stay hungy, Stay Foolish"
         content.body = "If today were the last day of your life, would you want to do what you are about to do today?"
         content.sound = .default
         content.badge = 1
 
-        
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInterval, repeats: repeats)
-        
-        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        return content
+    }
+    
+    private func sendNotification(content: UNMutableNotificationContent, trigger: UNNotificationTrigger) {
+        let request = UNNotificationRequest(identifier: UUID().uuidString,
+                                            content: content,
+                                            trigger: trigger)
         
         UNUserNotificationCenter.current().add(request)
         
